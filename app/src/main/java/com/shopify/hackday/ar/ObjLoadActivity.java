82e42bc1b66b531da9962487f75ddd329d115c58
Etienne Caron <etienne.caron@gmail.com>
2016-06-09 10:55:39 -0400

    Empty activity

---
+++ app/src/main/java/com/shopify/hackday/ar/ObjLoadActivity.java
@@ -0,0 +1,14 @@
+package com.shopify.hackday.ar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObjLoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obj_load);
+        
+    }
+}
