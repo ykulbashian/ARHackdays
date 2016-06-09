751d2a6325511adee7933644ba7e90018859bbba
Yervant <yervant.kulbashian@shopify.com>
2016-06-09 15:07:10 -0400

    Switched it back to main activity.

---
+++ app/src/main/java/com/shopify/hackday/ar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shopify.hackday.ar;
+
+import android.os.Bundle;
+
+import com.shopify.hackday.ar.vuforia.app.VirtualButtons.VirtualButtons;
+
+
+public class MainActivity extends VirtualButtons {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
