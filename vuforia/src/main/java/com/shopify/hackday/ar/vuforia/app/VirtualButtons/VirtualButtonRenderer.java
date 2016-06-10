1fd5b95a8f00659d76e336ea6065e8da9165405f
Yervant <yervant.kulbashian@shopify.com>
2016-06-09 20:18:00 -0400

    Started removing unnecessary stuff from renderer, and creating new replacement.

--- vuforia/src/main/java/com/shopify/hackday/ar/vuforia/app/VirtualButtons/VirtualButtonRenderer.java
+++ vuforia/src/main/java/com/shopify/hackday/ar/vuforia/app/VirtualButtons/VirtualButtonRenderer.java
@@ -64,14 +64,6 @@ public class VirtualButtonRenderer implements GLSurfaceView.Renderer
     private int mvpMatrixHandle = 0;
     private int texSampler2DHandle = 0;
     
-    private int lineOpacityHandle = 0;
-    private int lineColorHandle = 0;
-    private int mvpMatrixButtonsHandle = 0;
-    
-    // OpenGL ES 2.0 specific (Virtual Buttons):
-    private int vbShaderProgramID = 0;
-    private int vbVertexHandle = 0;
-    
     // Constants:
     static private float kTeapotScale = 3.f;
     
@@ -191,19 +183,7 @@ public class VirtualButtonRenderer implements GLSurfaceView.Renderer
             "modelViewProjectionMatrix");
         texSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID,
             "texSampler2D");
-        
-        // OpenGL setup for Virtual Buttons
-        vbShaderProgramID = SampleUtils.createProgramFromShaderSrc(
-            LineShaders.LINE_VERTEX_SHADER, LineShaders.LINE_FRAGMENT_SHADER);
-        
-        mvpMatrixButtonsHandle = GLES20.glGetUniformLocation(vbShaderProgramID,
-            "modelViewProjectionMatrix");
-        vbVertexHandle = GLES20.glGetAttribLocation(vbShaderProgramID,
-            "vertexPosition");
-        lineOpacityHandle = GLES20.glGetUniformLocation(vbShaderProgramID,
-            "opacity");
-        lineColorHandle = GLES20.glGetUniformLocation(vbShaderProgramID,
-            "color");
+
     }
     
     
@@ -259,123 +239,6 @@ public class VirtualButtonRenderer implements GLSurfaceView.Renderer
             // Set the texture used for the teapot model:
             int textureIndex = 0;
             
-            float vbVertices[] = new float[imageTargetResult
-                .getNumVirtualButtons() * 24];
-            short vbCounter = 0;
-            
-            // Iterate through this targets virtual buttons:
-            for (int i = 0; i < imageTargetResult.getNumVirtualButtons(); ++i)
-            {
-                VirtualButtonResult buttonResult = imageTargetResult
-                    .getVirtualButtonResult(i);
-                VirtualButton button = buttonResult.getVirtualButton();
-                
-                int buttonIndex = 0;
-                // Run through button name array to find button index
-                for (int j = 0; j < VirtualButtonsActivity.NUM_BUTTONS; ++j)
-                {
-                    if (button.getName().compareTo(
-                        mActivity.virtualButtonColors[j]) == 0)
-                    {
-                        buttonIndex = j;
-                        break;
-                    }
-                }
-                
-                // If the button is pressed, than use this texture:
-                if (buttonResult.isPressed())
-                {
-                    textureIndex = buttonIndex + 1;
-                }
-                
-                Area vbArea = button.getArea();
-                assert (vbArea.getType() == Area.TYPE.RECTANGLE);
-                Rectangle vbRectangle[] = new Rectangle[4];
-                vbRectangle[0] = new Rectangle(-108.68f, -53.52f, -75.75f,
-                    -65.87f);
-                vbRectangle[1] = new Rectangle(-45.28f, -53.52f, -12.35f,
-                    -65.87f);
-                vbRectangle[2] = new Rectangle(14.82f, -53.52f, 47.75f, -65.87f);
-                vbRectangle[3] = new Rectangle(76.57f, -53.52f, 109.50f,
-                    -65.87f);
-                
-                // We add the vertices to a common array in order to have one
-                // single
-                // draw call. This is more efficient than having multiple
-                // glDrawArray calls
-                vbVertices[vbCounter] = vbRectangle[buttonIndex].getLeftTopX();
-                vbVertices[vbCounter + 1] = vbRectangle[buttonIndex]
-                    .getLeftTopY();
-                vbVertices[vbCounter + 2] = 0.0f;
-                vbVertices[vbCounter + 3] = vbRectangle[buttonIndex]
-                    .getRightBottomX();
-                vbVertices[vbCounter + 4] = vbRectangle[buttonIndex]
-                    .getLeftTopY();
-                vbVertices[vbCounter + 5] = 0.0f;
-                vbVertices[vbCounter + 6] = vbRectangle[buttonIndex]
-                    .getRightBottomX();
-                vbVertices[vbCounter + 7] = vbRectangle[buttonIndex]
-                    .getLeftTopY();
-                vbVertices[vbCounter + 8] = 0.0f;
-                vbVertices[vbCounter + 9] = vbRectangle[buttonIndex]
-                    .getRightBottomX();
-                vbVertices[vbCounter + 10] = vbRectangle[buttonIndex]
-                    .getRightBottomY();
-                vbVertices[vbCounter + 11] = 0.0f;
-                vbVertices[vbCounter + 12] = vbRectangle[buttonIndex]
-                    .getRightBottomX();
-                vbVertices[vbCounter + 13] = vbRectangle[buttonIndex]
-                    .getRightBottomY();
-                vbVertices[vbCounter + 14] = 0.0f;
-                vbVertices[vbCounter + 15] = vbRectangle[buttonIndex]
-                    .getLeftTopX();
-                vbVertices[vbCounter + 16] = vbRectangle[buttonIndex]
-                    .getRightBottomY();
-                vbVertices[vbCounter + 17] = 0.0f;
-                vbVertices[vbCounter + 18] = vbRectangle[buttonIndex]
-                    .getLeftTopX();
-                vbVertices[vbCounter + 19] = vbRectangle[buttonIndex]
-                    .getRightBottomY();
-                vbVertices[vbCounter + 20] = 0.0f;
-                vbVertices[vbCounter + 21] = vbRectangle[buttonIndex]
-                    .getLeftTopX();
-                vbVertices[vbCounter + 22] = vbRectangle[buttonIndex]
-                    .getLeftTopY();
-                vbVertices[vbCounter + 23] = 0.0f;
-                vbCounter += 24;
-                
-            }
-            
-            // We only render if there is something on the array
-            if (vbCounter > 0)
-            {
-                // Render frame around button
-                GLES20.glUseProgram(vbShaderProgramID);
-                
-                GLES20.glVertexAttribPointer(vbVertexHandle, 3,
-                    GLES20.GL_FLOAT, false, 0, fillBuffer(vbVertices));
-                
-                GLES20.glEnableVertexAttribArray(vbVertexHandle);
-                
-                GLES20.glUniform1f(lineOpacityHandle, 1.0f);
-                GLES20.glUniform3f(lineColorHandle, 1.0f, 1.0f, 1.0f);
-                
-                GLES20.glUniformMatrix4fv(mvpMatrixButtonsHandle, 1, false,
-                    modelViewProjection, 0);
-                
-                // We multiply by 8 because that's the number of vertices per
-                // button
-                // The reason is that GL_LINES considers only pairs. So some
-                // vertices
-                // must be repeated.
-                GLES20.glDrawArrays(GLES20.GL_LINES, 0,
-                    imageTargetResult.getNumVirtualButtons() * 8);
-                
-                SampleUtils.checkGLError("VirtualButtonsActivity drawButton");
-                
-                GLES20.glDisableVertexAttribArray(vbVertexHandle);
-            }
-            
             // Assumptions:
             assert (textureIndex < mTextures.size());
             Texture thisTexture = mTextures.get(textureIndex);
@@ -424,24 +287,6 @@ public class VirtualButtonRenderer implements GLSurfaceView.Renderer
         GLES20.glDisable(GLES20.GL_DEPTH_TEST);
         
         Renderer.getInstance().end();
-        
-    }
-    
-    
-    private Buffer fillBuffer(float[] array)
-    {
-        // Convert to floats because OpenGL doesnt work on doubles, and manually
-        // casting each input value would take too much time.
-        ByteBuffer bb = ByteBuffer.allocateDirect(4 * array.length); // each
-                                                                     // float
-                                                                     // takes 4
-                                                                     // bytes
-        bb.order(ByteOrder.LITTLE_ENDIAN);
-        for (float d : array)
-            bb.putFloat(d);
-        bb.rewind();
-        
-        return bb;
         
     }
     
