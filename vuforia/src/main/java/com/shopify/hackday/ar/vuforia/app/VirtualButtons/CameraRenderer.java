03c7db875936be054072e50dbdff3e98731ad69b
Yervant <yervant.kulbashian@shopify.com>
2016-06-16 13:36:07 -0400

    Removing unnecessary renderer code.

--- vuforia/src/main/java/com/shopify/hackday/ar/vuforia/app/VirtualButtons/CameraRenderer.java
+++ vuforia/src/main/java/com/shopify/hackday/ar/vuforia/app/VirtualButtons/CameraRenderer.java
@@ -62,20 +62,6 @@ public class CameraRenderer extends org.rajawali3d.renderer.Renderer implements 
     // The textures we will use for rendering:
     private Vector<Texture> mTextures;
 
-    private CubeObject mTeapot = new CubeObject();
-
-    // OpenGL ES 2.0 specific (3D model):
-    private int shaderProgramID = 0;
-    private int vertexHandle = 0;
-    private int normalHandle = 0;
-    private int textureCoordHandle = 0;
-    private int mvpMatrixHandle = 0;
-    private int texSampler2DHandle = 0;
-
-    // Constants:
-    static private float kTeapotScale = 5f;
-
-
     public CameraRenderer(VirtualButtonsActivity activity,
                           SampleApplicationSession session) {
         super(activity);
@@ -145,8 +131,6 @@ public class CameraRenderer extends org.rajawali3d.renderer.Renderer implements 
         if (!mIsActive)
             return;
 
-        // Call our function to render content
-        renderFrame();
     }
 
 
@@ -169,123 +153,7 @@ public class CameraRenderer extends org.rajawali3d.renderer.Renderer implements 
                     GLES20.GL_UNSIGNED_BYTE, t.mData);
         }
 
-        shaderProgramID = SampleUtils.createProgramFromShaderSrc(
-                CubeShaders.CUBE_MESH_VERTEX_SHADER,
-                CubeShaders.CUBE_MESH_FRAGMENT_SHADER);
-
-        vertexHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexPosition");
-        normalHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexNormal");
-        textureCoordHandle = GLES20.glGetAttribLocation(shaderProgramID,
-                "vertexTexCoord");
-        mvpMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID,
-                "modelViewProjectionMatrix");
-        texSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID,
-                "texSampler2D");
-
     }
-
-
-    private void renderFrame() {
-        // Clear color and depth buffer
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-        // Get the state from Vuforia and mark the beginning of a rendering section
-        State state = Renderer.getInstance().begin();
-
-        // Explicitly render the Video Background
-        Renderer.getInstance().drawVideoBackground();
-
-        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
-
-        // We must detect if background reflection is active and adjust the
-        // culling direction.
-        // If the reflection is active, this means the post matrix has been
-        // reflected as well,
-        // therefore counter standard clockwise face culling will result in
-        // "inside out" models.
-        GLES20.glEnable(GLES20.GL_CULL_FACE);
-        GLES20.glCullFace(GLES20.GL_BACK);
-        if (Renderer.getInstance().getVideoBackgroundConfig().getReflection() == VIDEO_BACKGROUND_REFLECTION.VIDEO_BACKGROUND_REFLECTION_ON)
-            GLES20.glFrontFace(GLES20.GL_CW); // Front camera
-        else
-            GLES20.glFrontFace(GLES20.GL_CCW); // Back camera
-
-        // Set the viewport
-        int[] viewport = vuforiaAppSession.getViewport();
-        GLES20.glViewport(viewport[0], viewport[1], viewport[2], viewport[3]);
-
-        // Did we find any trackables this frame?
-        if (state.getNumTrackableResults() > 0) {
-            // Get the trackable:
-            TrackableResult trackableResult = state.getTrackableResult(0);
-            float[] modelViewMatrix = Tool.convertPose2GLMatrix(trackableResult.getPose()).getData();
-
-            // The image target specific result:
-            assert (trackableResult.getType() == ImageTargetResult
-                    .getClassType());
-            ImageTargetResult imageTargetResult = (ImageTargetResult) trackableResult;
-
-            // Set transformations:
-            float[] modelViewProjection = new float[16];
-            Matrix.multiplyMM(modelViewProjection, 0, vuforiaAppSession
-                    .getProjectionMatrix().getData(), 0, modelViewMatrix, 0);
-
-            // Set the texture used for the teapot model:
-            int textureIndex = 0;
-
-            // Assumptions:
-            assert (textureIndex < mTextures.size());
-            Texture thisTexture = mTextures.get(textureIndex);
-
-            // Scale 3D model
-            float[] modelViewScaled = modelViewMatrix;
-            Matrix.scaleM(modelViewScaled, 0, kTeapotScale, kTeapotScale, kTeapotScale);
-
-            float[] modelViewProjectionScaled = new float[16];
-            Matrix.multiplyMM(modelViewProjectionScaled, 0, vuforiaAppSession
-                    .getProjectionMatrix().getData(), 0, modelViewScaled, 0);
-
-            // Render 3D model
-            GLES20.glUseProgram(shaderProgramID);
-
-            GLES20.glVertexAttribPointer(vertexHandle, 3, GLES20.GL_FLOAT,
-                    false, 0, mTeapot.getVertices());
-            GLES20.glVertexAttribPointer(normalHandle, 3, GLES20.GL_FLOAT,
-                    false, 0, mTeapot.getNormals());
-            GLES20.glVertexAttribPointer(textureCoordHandle, 2,
-                    GLES20.GL_FLOAT, false, 0, mTeapot.getTexCoords());
-
-            GLES20.glEnableVertexAttribArray(vertexHandle);
-            GLES20.glEnableVertexAttribArray(normalHandle);
-            GLES20.glEnableVertexAttribArray(textureCoordHandle);
-
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,
-                    thisTexture.mTextureID[0]);
-            GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false,
-                    modelViewProjectionScaled, 0);
-            GLES20.glUniform1i(texSampler2DHandle, 0);
-            GLES20.glDrawElements(GLES20.GL_TRIANGLES,
-                    mTeapot.getNumObjectIndex(), GLES20.GL_UNSIGNED_SHORT,
-                    mTeapot.getIndices());
-
-            GLES20.glDisableVertexAttribArray(vertexHandle);
-            GLES20.glDisableVertexAttribArray(normalHandle);
-            GLES20.glDisableVertexAttribArray(textureCoordHandle);
-
-            SampleUtils.checkGLError("VirtualButtonsActivity renderFrame");
-
-        }
-
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
-        Renderer.getInstance().end();
-
-    }
-
-
 
 
     class FloatingPoint {
